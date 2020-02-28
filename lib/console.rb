4533a0af11f173793ff148a4f54af7fc08abb306
Angel Szymczak <angel.szymczak@tunubi.com>
2020-02-28 18:49:24 -0300

    App login

--- lib/console.rb
+++ lib/console.rb
@@ -4,6 +4,10 @@ require 'console/environment'
 require 'digest'
 require 'console/key_generator'
 
+require 'io/console'
+require 'ext/string'
+require 'console/prompt'
+
 module Console
 end
 
--- lib/console/prompt.rb
+++ lib/console/prompt.rb
@@ -1,6 +1,26 @@
 module Console
   class Prompt
     def self.run!(options)
+      setup(options[:persisted])
     end
+
+    def self.setup(persist_file)
+      app_login
+    end
+
+    def self.app_login
+      loop do
+        print "Please enter #{ENV.fetch('COMPANY_NAME')} App Key: ".important
+        key = STDIN.noecho(&:gets).chomp
+        if ENV.fetch('APP_KEY') == key
+          puts 'App logged'.success
+          break
+        else
+          puts 'Invalid Key App'.error
+        end
+      end
+    end
+
+    private_class_method :setup, :app_login
   end
 end
